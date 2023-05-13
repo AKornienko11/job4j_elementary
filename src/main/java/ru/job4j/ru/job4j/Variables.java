f51fc42c8f1770a4d77d94d68a87f24442cc7bbf
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-13 23:18:16 +0500

    0. Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/ru/job4j/Variables.java
@@ -0,0 +1,12 @@
+package ru.job4j.ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float lenght = 78.0332F;
+        System.out.println(lenght);
+        boolean bady = true;
+        System.out.println(bady);
+    }
+}
