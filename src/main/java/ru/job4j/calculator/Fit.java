6eb5274def48c4cc00dfbf46e5f0f6914aabbc0a
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-19 18:47:41 +0500

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(int height) {
+       double rsl = ((height - 100) * 1.15);
+        return rsl;
+    }
+
+    public static double womanWeight(int height) {
+        double rsl = ((height - 110) * 1.15);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double man = manWeight(187);
+        System.out.println(" Man 187 is " + man);
+        double woman = womanWeight(187);
+        System.out.println(" Woman 187 is " + woman);
+    }
+}
