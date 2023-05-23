2ae75dc9515e03f86a67b98431d24534195eacc6
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-23 23:27:06 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 System.out.println("Aleksey Kornienko");
 System.out.println("11.07.1990");
     }
