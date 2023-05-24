7680e2aae2d29ac3b4d842f9605a398d587731fb
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-24 17:00:18 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,5 +1,8 @@
 package ru.job4j;
 
+/*
+устранил пробелы для успешного прохождения задания
+ */
 public class Studentinfo {
     public static void main(String[] args) {
 System.out.println("Aleksey Kornienko");
