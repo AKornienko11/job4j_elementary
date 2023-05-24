0fee05e8796560aa48bb342d7a88c653746578ab
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-24 16:54:55 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,6 +1,11 @@
 package ru.job4j;
 
+/*
+устранил пробелы для успешного прохождения задания
+ */
+
 public class Info {
+
     public static void main(String[] args) {
         System.out.println("10.05.2023");
     }
