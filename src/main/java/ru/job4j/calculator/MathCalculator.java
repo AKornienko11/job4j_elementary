9b1cefc0efaa41ac4c55881aaf589697358ac4ae
AKornienko3 <Kornienko.srs@mail.ru>
2023-05-27 20:59:06 +0500

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -15,7 +15,7 @@ public class MathCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен : " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + divAndDiff(10, 20));
         System.out.println("Результат расчета равен: " + allVoid(10, 20));
     }
