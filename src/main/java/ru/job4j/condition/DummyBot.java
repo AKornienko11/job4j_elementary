67340648b67e66289bd658e6afbdd5532e76997e
AKornienko3 <Kornienko.srs@mail.ru>
2023-06-03 18:34:56 +0500

    4.1.2. Глупый бот  [#31323 #419882]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,10 +5,7 @@ public class DummyBot {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
             rsl = "Hi, SmartAss.";
-        } else if ("Can you add two plus two?".equals(question)) {
-            rsl = " I don't know. Please, ask another question.";
-        }
-        if ("Bye.".equals(question)) {
+        } else if ("Bye.".equals(question)) {
             rsl = "See you later.";
         }
         return rsl;
@@ -17,9 +14,9 @@ public class DummyBot {
     public static void main(String[] args) {
         String rsl = DummyBot.answer("Hi, Bot.");
         System.out.println(rsl);
-        rsl = DummyBot.answer(" Can you add two plus two?");
-        System.out.println(rsl);
         rsl = DummyBot.answer("Bye.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer(" Can you add two plus two?");
         System.out.println(rsl);
     }
 }
