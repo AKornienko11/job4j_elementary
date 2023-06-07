fb5940a356f9705c15ed850cb1e5cd2d47dc1b42
AKornienko3 <Kornienko.srs@mail.ru>
2023-06-07 20:57:22 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Aleksey Kornienko";
+        names[1] = "Artem Bozhok";
+        names[2] = "Aleksey Bozhok";
+        names[3] = "Viktoria Klimova";
+        String names1 = names[0];
+        String names2 = names[1];
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println(names1);
+        System.out.println(names2);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
