92d01f1a34b6400ee3d16612aa78c19789a75a5c
AKornienko3 <Kornienko.srs@mail.ru>
2023-06-24 13:02:47 +0500

    6.7.1. Моно строка в матрице. [#214126 #425046]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,12 +3,10 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = row; i < row + 1; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == ' ') {
-                    result = false;
-                    break;
-                }
+        for (int j = row; j < board.length; j++) {
+            if (board[row][j] != 'X') {
+                result = false;
+                break;
             }
         }
         return result;
@@ -17,11 +15,9 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int colum) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            for (int j = colum; j <= colum; j++) {
-                if (board[i][j] != 'X') {
-                    result = false;
-                    break;
-                }
+            if (board[i][colum] != 'X') {
+                result = false;
+                break;
             }
         }
         return result;
@@ -33,6 +29,17 @@ public class MatrixCheck {
             rsl[i] = board[i][i];
         }
         return rsl;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
 
