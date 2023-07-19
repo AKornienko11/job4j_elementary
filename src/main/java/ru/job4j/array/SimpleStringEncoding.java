36c1da925a314cbba44c180b6a30c923fb4c692d
AKornienko3 <Kornienko.srs@mail.ru>
2023-07-19 13:27:21 +0500

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/SimpleStringEncoding.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoding.java
@@ -1,20 +1,20 @@
 package ru.job4j.array;
 
-public class SimpleStringEncoder {
+public class SimpleStringEncoding {
     public static String encode(String input) {
         String result = "";
         char symbol = input.charAt(0);
-        int counter = 1;
+        int count = 1;
         for (int i = 1; i < input.length(); i++) {
             if (symbol == input.charAt(i)) {
-                counter++;
+                count++;
             } else {
-                result = counter > 1 ? result + symbol + counter : result + symbol;
+                result = count > 1 ? result + symbol + count : result + symbol;
                 symbol = input.charAt(i);
-                counter = 1;
+                count = 1;
             }
         }
-        return (counter > 1) ? result + symbol + counter : result + symbol;
+        return (count > 1) ? result + symbol + count : result + symbol;
     }
 }
 
--- src/test/java/ru/job4j/array/SimpleStringEncoderTest.java
+++ src/test/java/ru/job4j/array/SimpleStringEncoderTest.java
@@ -9,7 +9,7 @@ class SimpleStringEncoderTest {
     void whenStringaaabbcThenResulta3b2c() {
         String input = "aaabbc";
         String expected = "a3b2c";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -17,7 +17,7 @@ class SimpleStringEncoderTest {
     void whenStringabbcccThenResulta2b3c() {
         String input = "abbccc";
         String expected = "ab2c3";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -25,7 +25,7 @@ class SimpleStringEncoderTest {
     void whenStringaaabccThenResulta3bc2() {
         String input = "aaabcc";
         String expected = "a3bc2";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -33,7 +33,7 @@ class SimpleStringEncoderTest {
     void whenStringabcThenResultabc() {
         String input = "abc";
         String expected = "abc";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -41,7 +41,7 @@ class SimpleStringEncoderTest {
     void whenStringaaabbbcccThenResulta3b3c3() {
         String input = "aaabbbccc";
         String expected = "a3b3c3";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -49,7 +49,7 @@ class SimpleStringEncoderTest {
     void whenStringaaaaaaaaaaaabbbcddddThenResulta12b3cd4() {
         String input = "aaaaaaaaaaaabbbcdddd";
         String expected = "a12b3cd4";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -57,7 +57,7 @@ class SimpleStringEncoderTest {
     void whenStringaaabbbbaaabbcccdddddThenResulta3b4a3b2c3d5() {
         String input = "aaabbbbaaabbcccddddd";
         String expected = "a3b4a3b2c3d5";
-        String result = SimpleStringEncoder.encode(input);
+        String result = SimpleStringEncoding.encode(input);
         assertThat(result).isEqualTo(expected);
     }
 }
