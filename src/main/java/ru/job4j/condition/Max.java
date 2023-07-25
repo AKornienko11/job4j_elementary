07c873f7e990f5ffe71858d9d54e52bc50094f38
AKornienko3 <Kornienko.srs@mail.ru>
2023-07-25 11:27:38 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,13 +1,21 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int maxi(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static int max(int left, int right, int forward) {
+        return max(max(left, right), forward);
+    }
+
+    public static int max(int left, int right, int forward, int back) {
+        return max(max(left, right), max(forward, back));
     }
 
     public static void main(String[] args) {
-        int result = Max.maxi(1, 2);
+        int result = Max.max(7, 2, 3);
         System.out.println(result);
+
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -10,25 +10,28 @@ class MaxTest {
         int left = 1;
         int right = 2;
         int expected = 2;
-        int result = Max.maxi(1, 2);
+        int result = Max.max(1, 2);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax2To1Then2() {
+    public void whenMax2To1To7Then7() {
         int left = 2;
         int right = 1;
-        int expected = 2;
-        int result = Max.maxi(2, 1);
+        int forward = 7;
+        int expected = 7;
+        int result = Max.max(2, 1, 7);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax2To2Then2() {
+    public void whenMax2To2To7To10Then10() {
         int left = 2;
         int right = 2;
-        int expected = 2;
-        int result = Max.maxi(2, 2);
+        int forward = 7;
+        int back = 10;
+        int expected = 10;
+        int result = Max.max(2, 2, 7, 10);
         assertThat(result).isEqualTo(expected);
     }
 }
