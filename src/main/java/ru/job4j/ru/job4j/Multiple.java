de55205db0e137f7b5068d73d27fc7063feb03dc
AKornienko3 <Kornienko.srs@mail.ru>
2024-07-27 20:07:46 +0500

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/ru/job4j/Multiple.java
@@ -1,19 +1,43 @@
 package ru.job4j.ru.job4j;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /*
 устранил пробелы для успешного прохождения задания
  */
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println(" 1 * 1 = 1 ");
-        System.out.println(" 1 * 2 = 2 ");
-        System.out.println(" 1 * 3 = 3 ");
-        System.out.println(" 1 * 4 = 4 ");
-        System.out.println(" 1 * 5 = 5 ");
-        System.out.println(" 1 * 6 = 6 ");
-        System.out.println(" 1 * 7 = 7 ");
-        System.out.println(" 1 * 8 = 8 ");
-        System.out.println(" 1 * 9 = 9 ");
-
+        try (FileOutputStream output = new FileOutputStream("C:/projects/job4j_design/data/result.txt")) {
+            System.out.println(" 1 * 1 = 1 ");
+            output.write("1".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 2 = 2 ");
+            output.write("2".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 3 = 3 ");
+            output.write("3".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 4 = 4 ");
+            output.write("4".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 5 = 5 ");
+            output.write("5".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 6 = 6 ");
+            output.write("6".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 7 = 7 ");
+            output.write("7".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 8 = 8 ");
+            output.write("8".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            System.out.println(" 1 * 9 = 9 ");
+            output.write("9".getBytes());
+            output.write(System.lineSeparator().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
